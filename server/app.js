de9810dad09941cd6fcf35d913533b8980afc56a
suhyeon-ju <jusu2529@gmail.com>
2022-08-10 23:18:04 +0900

    oracle db connection success

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie:{
-    secure: true,
+    secure: false,  // false 이면 session에 키값 넣어서 test 가능
     maxAge: 1000 * 60 * 60 //쿠키 유효시간 1시간
   }
 }));
@@ -27,6 +27,13 @@ const server = app.listen(3000, () => {
 // }
 // const dbPool = require('oracledb').getConnection(db);
 
+const dbPool = {
+  user: 'potato',
+  password: 'potato',
+  host: '192.168.0.1',
+  database: 'xe'
+}
+
 const oracledb = require('oracledb')
 oracledb.getConnection({
   user: 'potato',
@@ -39,14 +46,6 @@ oracledb.getConnection({
     return;
   }
   console.log('접속 성공');
-  conn.execute('select * from pr_prod_bas',function(err,result){
-    if(err){
-      console.log('쿼리 실패', err);
-      return;
-    }
-    console.log('쿼리 성공');
-    console.log(result.rows);
-  })
 }
 );
 
@@ -54,23 +53,28 @@ oracledb.getConnection({
 
 
 app.post('/api/login', async (request, res) => {
-
+  request.session['email'] = 'test@email.com';
+  res.send('ok');
 });
 
 app.post('/api/logout', async (request, res) => {
-
+  request.session.destroy();
+  res.send('ok');
 });
 
 const sql = require('./sql.js');
 
 app.post('/api/:alias', async(request, res) => {
+  if(!request.session.email){
+    return res.status(401).send({error:'You need to login.'});
+  }
   try { 
    
     // '/api/:alias'의 alias 자리에 오는 값이 아래에 들어가게 된다
      res.send(await req.db(request.params.alias));
   } catch(err){
     res.status(500).send({
-      error: err
+      error: request.params.alias
     });
   }
 });
@@ -78,14 +82,18 @@ app.post('/api/:alias', async(request, res) => {
 const req = {
   async db(alias, param = [], where = ''){
     console.log(sql[alias].query);
-    return new Promise((resolve, reject) => dbPool.execute(sql[alias].query + where, param, (error, rows) => {
-      if(error){
-        if (error.code != 'ER_DUP_ENTRY')
-          console.log(error);
-        resolve({
-          error
-        });
-      } else resolve(rows);
-    }));
+    return new Promise((resolve, reject) => oracledb.getConnection(dbPool,function(err,conn){
+      conn.execute(sql[alias].query + where, param, (error, rows) => {
+        if(error){
+          if (error.code != 'ER_DUP_ENTRY')
+            console.log(error);
+          resolve({
+            error
+          });
+        } else resolve(rows);
+      })
+
+    }
+     ));
   }
 };
---
+++ server/sql.js
@@ -0,0 +1,9 @@
+module.exports = {
+  productList : {
+    query: 'select * from pr_prod_bas'
+  },
+  sellerList : {
+    query: 'select * from se_seller_bas'
+  }
+
+}
